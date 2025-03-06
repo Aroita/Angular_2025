3ae927ed1180d0ddd9fb286174f590c51194480c
Aroa <aroitaim84@gmail.com>
2025-03-06 13:34:54 +0100

    55. agregar personajes al array

--- 02-bases/src/app/pages/dragonball/dragonball.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball.component.ts
@@ -24,6 +24,10 @@ interface Character {
 
 export class DragonballComponent {
 
+  //añadir signal a name y power
+  name = signal('Goku');
+  power = signal(1500);
+
   //2- inicializar el array de personajes de dragonball interface con signal
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 15000 },
@@ -38,6 +42,33 @@ export class DragonballComponent {
     { id: 10, name: 'Chaoz', power: 2000 },
 
   ]);
+
+  //
+  addCharacter() {
+    //condicion: si no hay nombre o poder o el poder es menor o igual a 0, NO se añade el personaje
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+
+    //PERO SI TENEMOS PODER (numero), añadimos un nuevo personaje al array de personajes
+    //crea un nuevo objeto de tipo Character que lo asigna a la constante newCharacter con los valores de name y power
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+//añade el nuevo personaje al array de personajes y visualiza en memoria no fijo
+    //this.characters().push(newCharacter);
+
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 
 
 
