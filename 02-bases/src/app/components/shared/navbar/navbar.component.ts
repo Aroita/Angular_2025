5d2fcea9927de2a5dd5c7352bdd1f0950141e9a9
Aroa <aroitaim84@gmail.com>
2025-03-06 10:59:37 +0100

    49 activar hover - [routerLinkActiveOptions]="{ exact: true }

--- 02-bases/src/app/components/shared/navbar/navbar.component.ts
+++ 02-bases/src/app/components/shared/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  color: string = 'blue';  //color inicial
 
+  constructor() {
+    setInterval(() => {
+    //array de colores
+    const colores = ['blue', 'red', 'green', 'yellow', 'orange', 'purple'];
+    //Math.random() genera un decimal aleatorio entre 0 y 1, lo multiplica por la longitud del array(colores) y
+    // Math.floor lo redondea entre 0 y 4
+    this.color = colores[Math.floor(Math.random() * colores.length)];
+    }, 2000);
+  }
 }
+
+
+
